3e04669c0f196fd02fae1fbaa4a9034060656367
Sohib <xolburiyev@gmail.com>
2026-06-19 17:40:13 +0500

    qwerty

--- conf/settings.py
+++ conf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d6447_-38u3#%q&7=94s!n$s+3=ixljl&l4x+f96)di!9zphit'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ro.pharmi.uz', 'www.ro.pharmi.uz', 'localhost', '127.0.0.1']
 
